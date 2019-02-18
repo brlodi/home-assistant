f20a2e6445bdea46ccca25e2d427cbe40cdfb781
Benjamin Lodi <brlodi@gmail.com>
2019-02-17 23:23:05 -0500

    Tweaks to trackers & new Python app levergaing it

--- appdaemon/apps/presence.py
+++
@@ -1,9 +0,0 @@
-import appdaemon.plugins.hass.hassapi as hass
-
-class PresenceActions(hass.Hass):
-    
-    def initialize(self):
-        self.listen_state(self.arrival, entity='group.all_devices', new='home')
-    
-    def arrival(self):
-        print('foo')
